f646604f1aa12059fd642e5f229792713d28733a
liyongqiang <liyongqiang>
2020-07-10 18:11:49 +0800

    修改文件名称

--- tc-trics-mall-eureka/src/main/java/com/manage/mall/TricsEurekaApplication.java
+++ tc-trics-mall-eureka/src/main/java/com/manage/mall/TricsEurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class TricsIdcEurekaApplication {
+public class TricsEurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TricsIdcEurekaApplication.class, args);
+        SpringApplication.run(TricsEurekaApplication.class, args);
     }
 }
